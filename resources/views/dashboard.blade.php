9e2fccad8fa72c69a6173511c354610e24ed4f76
Enrica Carpena <enricajulescarpena@gmail.com>
2023-02-28 10:55:18 +0800

    Update Chart.js CDN

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -101,6 +101,7 @@
 
 <script src="scripts/main.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.2.1/chart.min.js" integrity="sha512-v3ygConQmvH0QehvQa6gSvTE2VdBZ6wkLOlmK7Mcy2mZ0ZF9saNbbk19QeaoTHdWIEiTlWmrwAL4hS8ElnGFbA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-datalabels/2.0.0/chartjs-plugin-datalabels.js"></script>
 
 <script>
